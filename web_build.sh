460cb64350879c458d402746a1756d3c859d575a
Andrew Wheeler <awheeler294@gmail.com>
2024-08-31 12:41:29 -0600

    use coi-serviceworker to add headers so love.js doesnt have to build in compatibility mode

--- web_build.sh
+++ web_build.sh
@@ -1,2 +1,10 @@
-npx love.js -m 50331648 -c -t 'Space Attack!' src docs
-cp favicon.ico docs
+IN=src
+OUT=web
+rm -f "$OUT"/game.data
+npx love.js -m 50331648 -t 'Space Attack!' "$IN" "$OUT"
+cp favicon.ico "$OUT"
+
+npm i --save coi-serviceworker
+cp node_modules/coi-serviceworker/coi-serviceworker.js "$OUT"
+
+sed -i '/<body>/a \    <script src="coi-serviceworker.js"></script>\n' "$OUT"/index.html
